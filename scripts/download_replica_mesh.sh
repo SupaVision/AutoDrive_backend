74f951aaf7a21699c6eb4866c8f8057e57ba1401
atticuszz <1831768457@qq.com>
2024-02-26 19:57:43 +0800

    add conda init file of localization

---
+++ scripts/download_replica_mesh.sh
@@ -0,0 +1,6 @@
+# you can also download the cull_replica_mesh.zip manually through
+# link: https://caiyun.139.com/m/i?1A5CvGNQmSDsI  password: Dhhp
+mkdir -p Datasets
+cd Datasets
+wget https://cvg-data.inf.ethz.ch/nice-slam/cull_replica_mesh.zip
+unzip cull_replica_mesh.zip
---
+++ src/localization/ReplicaLoader.py
@@ -0,0 +1,12 @@
+import open3d as o3d
+
+# 替换为你的点云文件路径
+point_cloud_path = "/home/atticuszz/DevSpace/python/AutoDrive_backend/Datasets/Replica/room0_mesh.ply"
+pcd = o3d.io.read_point_cloud(point_cloud_path, print_progress=True)
+
+# 可视化点云
+o3d.visualization.draw_geometries([pcd])
+
+
+class Replica:
+    pass
