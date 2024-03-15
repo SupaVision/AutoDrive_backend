b327a2db944a929373e59b256d3f8712fb5ded57
atticuszz <1831768457@qq.com>
2024-03-15 13:25:28 +0800

    fix: Building wheel for diff-gaussian-rasterization (setup.py) ... error #81

--- src/localization/ReplicaLoader.py
+++ src/localization/ReplicaLoader.py
@@ -1,7 +1,7 @@
 import open3d as o3d
 
 # 替换为你的点云文件路径
-point_cloud_path = "/home/atticuszz/DevSpace/python/AutoDrive_backend/Datasets/Replica/room0_mesh.ply"
+point_cloud_path = "/home/atticuszz/DevSpace/python/AutoDrive_backend/Datasets/cull_replica_mesh/office4.ply"
 pcd = o3d.io.read_point_cloud(point_cloud_path, print_progress=True)
 
 # 可视化点云
